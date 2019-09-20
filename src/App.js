ef016d17b80b989860663f36676a74fe4dc11503
Mazdak Atighi <maz@truss.works>
2019-09-19 22:52:26 -0500

    add/clear/update count

--- src/App.js
+++ src/App.js
@@ -1,54 +1,86 @@
-import React from 'react';
+import React, {Component} from 'react'
 import './App.css';
 import 'todomvc-app-css/index.css';
 import 'todomvc-common/base.css';
 
-function App() {
-  return (
-    <section class="todoapp">
-      <header class="header">
-				<h1>todos</h1>
-				<input class="new-todo" placeholder="What needs to be done?" autofocus />
-			</header>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      inputValue: ""
+    };
+    this.createNewTodo = this.createNewTodo.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  pluralize(todos) {
+    return todos.length === 0 || todos.length > 1 ?
+      "items" :
+      "item";
+  }
 
-      <section class="main">
-        <input id="toggle-all" class="toggle-all" type="checkbox" />
-        <label for="toggle-all">Mark all as complete</label>
-        <ul class="todo-list">
-          <li class="completed">
-            <div class="view">
-              <input class="toggle" type="checkbox" checked />
-              <label>Taste Javascript</label>
-              <button class="destroy"></button>
-            </div>
-            <input class="edit" value="Create a TodoMVC template" />
-          </li>
+  generateId() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      // eslint-disable-next-line no-mixed-operators
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+
+  createNewTodo(event) {
+    if(event.key === "Enter" && event.target.value) {
+      const updatedTodos = [{value: event.target.value.trim(), completed: false, id: this.generateId()}].concat(this.state.todos);
+      this.setState({todos: updatedTodos, inputValue: ""});
+    }
+  }
+
+  handleChange(event) {
+    this.setState({inputValue: event.target.value});
+  }
+
+  render() {
+    return (
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input onKeyDown={this.createNewTodo} onChange={this.handleChange} className="new-todo" value={this.state.inputValue} placeholder="What is to be done?" autoFocus />
+        </header>
+  
+        <section className="main">
+          <input id="toggle-all" className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            {this.state.todos.map(todo =>
+            <li key={todo.id}>
+              <div className="view">
+                <input className="toggle" type="checkbox" {...todo.completed && "checked"}/>
+                <label>{todo.value}</label>
+                <button className="destroy"></button>
+              </div>
+              {/* <input className="edit" value="Create a TodoMVC template" /> */}
+            </li>
+            )}
+          </ul>
+        </section>
+
+        <footer className="footer">
+          <span className="todo-count">{this.state.todos.length} {this.pluralize(this.state.todos)} left</span>
+          <ul className="filters">
           <li>
-            <div class="view">
-              <input class="toggle" type="checkbox" />
-              <label>Buy a unicorn</label>
-              <button class="destroy" />
-            </div>
-            <input class="edit" value="Rule the web" />
-          </li>
-        </ul>
+              <a className="selected" href="#/">All</a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+        </footer>
       </section>
-      <footer class="footer">
-        <span class="todo-count">0 item left</span>
-        <ul class="filters">
-        <li>
-						<a class="selected" href="#/">All</a>
-					</li>
-					<li>
-						<a href="#/active">Active</a>
-					</li>
-					<li>
-						<a href="#/completed">Completed</a>
-					</li>
-        </ul>
-      </footer>
-    </section>
-  );
+    );
+  }
+
+  
 }
 
 export default App;
